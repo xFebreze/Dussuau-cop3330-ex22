7bbc56a980edd9810937e8d400dcd5d35cc98b32
Alek Dussuau <ajdussuau@gmail.com>
2021-06-06 22:40:14 -0400

    Initial commit

---
+++ src/main/java/Base/App.java
@@ -0,0 +1,30 @@
+package Base;
+/*
+UCF COP3330 Summer 2021 Assignment 1 Solution
+Copyright 2021 Alek Dussuau
+
+
+Exercise 22 - Comparing Numbers
+        Comparing one input to a known value is common enough, but you’ll often need to process a collection of inputs.
+
+        Write a program that asks for three numbers. Check first to see that all numbers are different. If they’re not different, then exit the program. Otherwise, display the largest number of the three.
+
+        Example Output
+        Enter the first number: 1
+        Enter the second number: 51
+        Enter the third number: 2
+        The largest number is 51.
+
+        Constraint
+        Write the algorithm manually. Don’t use a built-in function for finding the largest number in a list.
+
+        Challenges
+        Modify the program so that all entered values are tracked and the user is prevented from entering a number that’s already been entered.
+        Modify the program so that it asks for ten numbers instead of three.
+        Modify the program so that it asks for an unlimited number of numbers.
+*/
+public class App {
+    public static void main(String[] args){
+        
+    }
+}
